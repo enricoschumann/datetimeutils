ae7216f3ccfb056ed846599afc620a2838ea2c82
Enrico Schumann <es@enricoschumann.net>
2019-12-22 09:33:44 +0100

    Add vectors 'month.name.de' and 'month.abb.de.din1355.1'

---
+++ R/data.R
@@ -0,0 +1,12 @@
+month.name.de <- c("Januar", "Februar", "M\u00e4rz",
+                   "April", "Mai", "Juni",
+                   "Juli", "August", "September",
+                   "Oktober", "November", "Dezember")
+Encoding(month.name.de) <- "UTF-8"
+
+
+## uses abbreviations DIN 1355-1
+month.abb.de.din1355.1 <- c("Jan", "Feb", "Mrz",
+                            "Apr", "Mai", "Jun",
+                            "Jul", "Aug", "Sep",
+                            "Okt", "Nov", "Dez")
